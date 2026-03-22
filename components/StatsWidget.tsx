d769b34ad0c9bd2a3f0096cb6a0601bc59a846fd
himanshu79768 <boranahiman45@gmail.com>
2026-03-22 09:37:14 +0530

    Update StatsWidget.tsx

--- components/StatsWidget.tsx
+++ components/StatsWidget.tsx
@@ -71,7 +71,7 @@ const StatsWidget: React.FC = () => {
     const currentPage = statPages[page];
 
     return (
-        <div className="w-full h-full p-6 flex flex-col justify-between bg-light-card dark:bg-dark-card text-light-card-foreground dark:text-dark-card-foreground overflow-hidden">
+        <div className="w-full h-full p-6 flex flex-col justify-between text-light-card-foreground dark:text-dark-card-foreground overflow-hidden">
             <div className="flex justify-between items-center">
                 <h3 className="font-semibold text-left text-base flex items-center gap-2">
                     {currentPage.icon} {currentPage.title}
